2c6953a410be2aa9b52699806c29e4ba5e5638b3
Afzal79 <afzal.muhammad@magairports.com>
2019-10-26 16:12:19 +0100

    add clockwork package & send message function

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var clockwork = require('clockwork')({key:'84b3b39adaeacdb378a174c2fedb58cc62dcd01e'});
+
 
 app.get('/', (req, res) => res.json({key:  "value"}))
 
 app.post('/immediate-help', (req, res) => { /* talk to clockwork */ res.sendStatus(200) })
+
+app.get('/test', (req, res) => { res.send("Hello World") });
+
+app.get('/sendMessage', (req, res) => { 
+    clockwork.sendSms({ To: '447758941459', Content: 'Hello World'}, 
+        function(error, resp) {
+            if (error) {
+                console.log('Something went wrong', error);
+            } else {
+            console.log('Message sent',resp.responses[0].id);
+            }
+        });
+
+        res.send("The message was sent");
+});
+
+app.get('');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
