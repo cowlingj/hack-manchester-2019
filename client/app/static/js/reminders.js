c555bd6c84d610a6f4d7d7ffb38c0921ec6b0eca
cowlingj <09cowlingj@gmail.com>
2019-10-26 21:38:06 +0100

    Add ability to mark reminders as done

--- client/app/static/js/reminders.js
+++ client/app/static/js/reminders.js
@@ -1,3 +1,15 @@
+function markReminderAsDone(id) {
+  var request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:3000/reminders/done/" + id)
+  request.onreadystatechange = function () {
+    if(request.readyState === 4 && request.status === 200) {
+      const toRemove = document.getElementsByClassName('reminder-' + id)[0]
+      toRemove.parentNode.removeChild(toRemove)
+    }
+  }
+
+}
+
 var request = new XMLHttpRequest();
 request.open("GET", "http://localhost:3000/reminders")
 request.onreadystatechange = function () {
@@ -13,15 +25,19 @@ request.onreadystatechange = function () {
             var reminderTextElement = document.createElement("p");
             reminderTextElement.classList.add('col-8');
 
-            if (typeof response.reminders[i] === 'string') {
-              reminderTextElement.innerHTML = response.reminders[i];
-            } else {
-              reminderTextElement.innerHTML = response.reminders[i].message;
-            }
-
             var reminderButtonElement = document.createElement('button');
             reminderButtonElement.classList.add('col-4');
             reminderButtonElement.innerHTML = 'done';
+
+            if (typeof response.reminders[i] === 'string') {
+              reminderTextElement.innerHTML = response.reminders[i];
+            } else {
+              reminderElement.classList.add('reminder-' + response.reminders[i].id)
+              reminderTextElement.innerHTML = response.reminders[i].message;
+              reminderButtonElement.onclick = function () {
+                markReminderAsDone(response.reminders[i].id)
+              }
+            }
 
             var reminderListElement = document.getElementById("reminderList");
             reminderElement.appendChild(reminderTextElement);
