934604d9082f06c1abbe832a30b297c4e6d37b91
cowlingj <09cowlingj@gmail.com>
2019-10-26 21:14:12 +0100

    Add reminder list

--- client/app/static/js/reminders.js
+++ client/app/static/js/reminders.js
@@ -8,28 +8,23 @@ request.onreadystatechange = function () {
         const response = JSON.parse(request.responseText)
         for (var i = 0; i < response.reminders.length; i++) {
             var reminderElement = document.createElement("div");
+            reminderElement.classList.add('row', 'reminder');
+
             var reminderTextElement = document.createElement("p");
-            
+            reminderTextElement.classList.add('col-8');
             reminderTextElement.innerHTML = response.reminders[i];
+
+            var reminderButtonElement = document.createElement('button');
+            reminderButtonElement.classList.add('col-4');
+            reminderButtonElement.innerHTML = 'done';
+
             var reminderListElement = document.getElementById("reminderList");
             reminderElement.appendChild(reminderTextElement);
+            reminderElement.appendChild(reminderButtonElement);
             reminderListElement.appendChild(reminderElement); 
         }
     }
-    console.log(request.status)
-    console.log(request.responseText)
-    const response = JSON.parse(request.responseText)
-    for (var i = 0; i < response.reminders.length; i++) {
-        var reminderTextElement = document.createElement("p");
-        reminderTextElement.innerHTML = response.reminders[i];
-        var reminderListElement = document.getElementById("reminderList");
-        reminderListElement.appendChild(reminderTextElement); 
-    }
-
   }
 }
 request.onerror = function (_req, _err) { console.log(request.responseText) }
 request.send();
-
-
-
